6bf4c92378d6600b9f31f9900794d51c372e8330
ngxson <thichthat@gmail.com>
2023-05-19 14:01:07 +0200

    ci: add buildx

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -4,6 +4,7 @@
 # Also build one for :latest
 
 IMAGE="ngxson/portainer-ce-without-annoying"
+ARCHS="linux/amd64,linux/arm64,linux/arm/v7"
 
 if [ -z "$TAG" ]; then
   echo "Please set TAG environment variable"
@@ -17,9 +18,8 @@ if [ -z "$MULTIARCH" ]; then
   docker build -t "$IMAGE:$TAG" -f Dockerfile.tmp .
 else
   echo "Multi-arch build..."
-  docker build --platform linux/amd64 -t "$IMAGE:$TAG" -f Dockerfile.tmp .
-  docker build --platform linux/arm64 -t "$IMAGE:$TAG" -f Dockerfile.tmp .
-  docker build --platform linux/arm/v7 -t "$IMAGE:$TAG" -f Dockerfile.tmp .
+  docker buildx create --use desktop-linux
+  docker buildx build --platform=$ARCHS -t "$IMAGE:$TAG" -f Dockerfile.tmp .
 fi
 
 docker push "$IMAGE:$TAG"
